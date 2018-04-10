920f7e4a61af00198aee3b30e84d3624571a9f52
Marc Baldwin <marc.baldwin88@gmail.com>
2018-04-10 16:12:59 +0100

    Use extension function for applyConstraints

--- constraint/src/main/java/xyz/marcb/constraint/ConstraintLayoutExtensions.kt
+++ constraint/src/main/java/xyz/marcb/constraint/ConstraintLayoutExtensions.kt
@@ -3,9 +3,9 @@ package xyz.marcb.constraint
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 
-inline fun ConstraintLayout.applyConstraints(handler: (ConstraintSet) -> Unit) {
+inline fun ConstraintLayout.applyConstraints(handler: ConstraintSet.() -> Unit) {
     val constraintSet = ConstraintSet()
     constraintSet.clone(this)
-    handler(constraintSet)
+    handler.invoke(constraintSet)
     constraintSet.applyTo(this)
 }

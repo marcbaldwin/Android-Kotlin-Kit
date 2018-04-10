b239d81bbba57fbacbae849ebd0e2cd33f2860c4
Marc Baldwin <marc.baldwin88@gmail.com>
2018-04-10 17:12:31 +0100

    Add pixelsToSp convenience function

--- support/src/main/java/xyz/marcb/support/ContextExtensions.kt
+++ support/src/main/java/xyz/marcb/support/ContextExtensions.kt
@@ -5,3 +5,5 @@ import android.content.Context
 fun Int.dpToPixels(context: Context) = (this * context.resources.displayMetrics.density + 0.5f).toInt()
 
 fun Int.spToPixels(context: Context) = (this * context.resources.displayMetrics.scaledDensity + 0.5f).toInt()
+
+fun Float.pixelsToSp(context: Context) = this / context.resources.displayMetrics.scaledDensity
